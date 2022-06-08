86e954f49d44d8a06e6068207e2f8280599c5e04
Nikolai Khalatiani <65181600+NikolaiKhalatiani@users.noreply.github.com>
2022-06-08 20:55:48 +0400

    Create listfun.mli

---
+++ listfun.mli
@@ -0,0 +1,6 @@
+val hd : 'a list -> 'a 
+val tl : 'a list -> 'a list 
+val length : 'a list -> int
+val append : 'a list -> 'a list -> 'a list 
+val rev : 'a list -> 'a list
+val nth : 'a list -> int -> 'a
